852bcb6d7c6e3e1f34663acd7dca709e2558692c
James Gifford <jg.webdeveloper@gmail.com>
2017-02-28 23:54:19 -0800

    Add service provider and clarify namespace

---
+++ src/BlogServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace JamesGifford\Blog;
+
+use Illuminate\Support\ServiceProvider;
+
+class BlogServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
